8662a9562219e9775d8187ec086518b0aae5596d
Mahesh Pasupuleti <mapa@microsoft.com>
2018-11-05 19:50:47 -0800

    Added react router

--- services/webclient/src/App.tsx
+++ services/webclient/src/App.tsx
@@ -1,26 +1,51 @@
 import * as React from 'react';
 import { NavigationDrawer } from 'react-md';
-import './App.css';
+import Home from './Home';
+import { Route, Switch } from 'react-router-dom';
+import NavLink from './NavLink';
 
-import logo from './logo.svg';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
+
+const navItems = [{
+    exact: true,
+    label: 'Home',
+    to: '/',
+    icon: 'home',
+  }, {
+    label: 'Page 1',
+    to: '/page-1',
+    icon: 'bookmark',
+  }, {
+    label: 'Page 2',
+    to: '/page-2',
+    icon: 'donut_large',
+  }, {
+    label: 'Page 3',
+    to: '/page-3',
+    icon: 'flight_land',
+  }];
 
 class App extends React.Component {
   public render() {
     return (
-     <NavigationDrawer
-        toolbarTitle="react-md with create-react-app v2"
-        drawerTitle="react-app"
-      >
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-          </p>
-        </div>
-      </NavigationDrawer>
+      <Route
+        render={({ location }) => (
+          <NavigationDrawer
+            drawerTitle="react-md with CRA"
+            toolbarTitle="Welcome to react-md"
+            navItems={navItems.map(props => <NavLink {...props} key={props.to} />)}
+          >
+            <Switch key={location.key}>
+              <Route exact path="/" location={location} component={Home} />
+              <Route path="/page-1" location={location} component={Page1} />
+              <Route path="/page-2" location={location} component={Page2} />
+              <Route path="/page-3" location={location} component={Page3} />
+            </Switch>
+          </NavigationDrawer>
+        )}
+      />
     );
   }
 }
---
+++ services/webclient/src/Home.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Component } from 'react';
+import './Home.css';
+import logo from './logo.svg';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="home-header">
+          <img src={logo} className="home-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="home-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
